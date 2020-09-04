7c4c7be7cc9ba34c83cdf16e813894e32ff29cef
Jung Lee <lee.jung.s95@gmail.com>
2020-09-04 04:32:27 +0000

    actual working folder for the CLI-Project

---
+++ CLI-Project-Charity/lib/CLI/Project/Charity.rb
@@ -0,0 +1,10 @@
+require "CLI/Project/Charity/version"
+
+module CLI
+  module Project
+    module Charity
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
