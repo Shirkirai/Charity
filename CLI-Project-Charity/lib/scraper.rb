ad76fabcd1c5e14edde8b85115342447c0e27bd7
Jung Lee <lee.jung.s95@gmail.com>
2020-09-14 00:44:23 +0000

    added make charity site object

--- CLI-Project-Charity/lib/scraper.rb
+++ CLI-Project-Charity/lib/scraper.rb
@@ -13,4 +13,22 @@ class Scraper
     def scrape_charity_index
         self.get_page.css("ul.toptenlist a")
     end
+
+    def make_charity_sites
+      self.scrape_charity_index.each do |r|
+        link = r.attr("href")
+        link_name = r.text
+
+
+end
+
+def make_charity_sites
+  self.scrape_charity_index.each do |link|
+  end
+end
+
+
+things.each do |r|
+  detail = "#{r.text}; link: #{r.attr("href")}"
+  puts detail
 end
