947ccec11c2cf03f681665cc171e9e6560705e1a
Jung Lee <lee.jung.s95@gmail.com>
2020-09-21 02:40:26 +0000

    added CLI file and updated scraper object with new charity site

--- CLI-Project-Charity/lib/scraper.rb
+++ CLI-Project-Charity/lib/scraper.rb
@@ -4,23 +4,39 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-class Top10Charity::Scraper
+#class Top10Charity::Scraper
 
+class Scraper
+  
+  
     def get_page
-        Nokogiri::HTML(open("https://www.charitynavigator.org/index.cfm?bay=topten"))
+        Nokogiri::HTML(open("https://www.charitywatch.org/top-rated-charities/all-charities"))
     end
 
     def scrape_charity_index
-        self.get_page.css("ul.toptenlist a")
+        self.get_page.css("#list-all")
+    end
+    
+    def scrape_charity_sites
+      self.get_page.css("#list-all .mb-4 a")
     end
 
     def make_charity_sites
-      self.scrape_charity_index.each do |r|
-        Top10Charity::Charities.new(r)
-        #note to self = research into "from_index_page" function...
-        #link = r.attr("href")
-        #link_name = r.text
+      #self.scrape_charity_index.each do |r|
+        #charity_titles = r.css(".mb-3").text
+      #end
+      self.scrape_charity_sites.each do |s|
+        charity_site = s.attr("href")
+        #this will produce a list of all the htmls
       end
     end
+    #def make_charity_sites
+      #self.scrape_charity_index.each do |r|
+        #Top10Charity::Charities.new(r)
+        #note to self = research into "from_index_page" function...
+       # link = r.attr("href")
+        #link_name = r.text
+     # end
+    #end
 
 end
